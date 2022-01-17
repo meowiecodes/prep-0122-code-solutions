1ed25c6595ec7544219e87c16abf8f7923b5eb8a
meowiecodes <86108768+meowiecodes@users.noreply.github.com>
2022-01-17 21:22:44 +0800

    Completed prep-javascript-objects - Charlene Singson

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,17 @@
+var person = { firstName: 'Sushi ', lastName: 'Cat', hobby: 'likes to demand' };
+
+console.log(person);
+
+var fullName = person.firstName + person.lastName;
+
+console.log("This person's full name is: " + fullName);
+
+person.job = 'President of Diva Company';
+
+console.log("The person's current job is: " + person.job);
+
+person.previousJob = 'Meowdel';
+
+console.log("The person's previous job is: " + person.previousJob);
+
+console.log(person);
