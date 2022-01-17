bc2ca1a5d90f5d4fc56d95aa6a2270c440ec7d9c
meowiecodes <86108768+meowiecodes@users.noreply.github.com>
2022-01-18 01:24:52 +0800

    changed lastElement formula

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -10,5 +10,5 @@ console.log('The third element of the array is ' + thirdElement);
 var length = languages.length;
 console.log('The length of the array is ' + length);
 
-var lastElement = languages[5];
+var lastElement = languages[length - 1];
 console.log('The last element of the array is ' + lastElement);
